b48e1ef630d0843c16f677a5bf4ba2bb72694b31
tpeele810 <tiffany.peele1@gmail.com>
2025-05-08 15:27:17 -0400

    thirteenth commit

---
+++ models/user.mjs
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+// Hash password before saving
+userSchema.pre('save', async function () {
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+});
+
+// Compare password method
+userSchema.methods.comparePassword = function (candidate) {
+  return bcrypt.compare(candidate, this.password);
+};
+
+export default mongoose.model('User', userSchema);
