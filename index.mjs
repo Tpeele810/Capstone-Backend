ad4a58eaf3b06d56be7655983aba31ad8e32d59e
tpeele810 <tiffany.peele1@gmail.com>
2025-05-08 11:13:33 -0400

    tenth commit

--- index.mjs
+++ index.mjs
@@ -8,3 +8,15 @@ import Application from './models/jobApplication.mjs'
 
 const app = express()
 const port = process.env.PORT || 3000
+
+//import routes
+import applications from './routes/applications.mjs'
+
+//middleware
+
+app.use(express.urlencoded())
+app.use(express.json())
+app.use(cors())
+
+
+app.use('/applications', applications)
