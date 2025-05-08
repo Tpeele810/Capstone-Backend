9e84b993477cd2fde2b34a263fd88b97df87243e
tpeele810 <tiffany.peele1@gmail.com>
2025-05-08 11:14:22 -0400

    eleventh commit

--- index.mjs
+++ index.mjs
@@ -20,3 +20,42 @@ app.use(cors())
 
 
 app.use('/applications', applications)
+
+
+import fs from 'fs'
+
+//Read the Json file
+fs.readFile('/data/seed.json', 'utf8', (err, data)=>{
+    if(err){
+        console.log ('Error reading the file:', err)
+        return
+    }
+    //Parse the Json data
+    const seed = Json.parse(data)
+
+    //use the seed data
+    console.log(seed)
+
+    //mogoose
+    mongoose.connect(process.env.ATLAS_URI)
+    .then (()=>{
+        console.log('Database connected')
+
+        //Insert applications
+        Application.insertMany(seed.applications)
+        .then(()=>console.log('Applications seeded'))
+        .catch(err => console.log('Error seeding', err))
+
+    })
+})
+
+//routes
+
+app.get('/', (req,res)=>{
+    res.send('Welcome to My Job Application Tracker')
+
+})
+
+app.listen(port, ()=>{
+    console.log(`Server is running on port ${port}`)
+})
